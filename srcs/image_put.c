82fd92426da32825b23166e43cf2c5e71d5e19e4
Real Madness <madness@CraSh.AnD.BurN>
2021-09-06 14:33:39 +0100

    makefile not working

--- srcs/image_put.c
+++ srcs/image_put.c
@@ -3,7 +3,7 @@
 void	image_put(char symb, t_vars *d, int y, int x)
 {
 	if (symb == '1' || symb == '0' || symb == 'C' || symb == 'E')
-		//put_inactive_image();
+		put_static_image();
 	if(symb == 'P' || symb == 'D' || symb == 'X')
 	{
 		if (symb == 'P')
@@ -11,16 +11,18 @@ void	image_put(char symb, t_vars *d, int y, int x)
 			d->pos_x = x;
 			d->pos_y = y;
 		}
+		put_moving_image()
 	}
+	mlx_put_image_to_window()
 }
 
 void	put_static_image(char symb, t_vars *d)
 {
 	if (symb == '1')
-		d->imag.img_border = mlx_xpm_file_to_image(d->mlx, d->imag.img_border, \
+		d->img = mlx_xpm_file_to_image(d->mlx, d->imag.img_border, \
 									&d->imag.img_wid, &d->imag.img_hei);
 	if (symb == '0')
-		d->imag.img_ground = mlx_xpm_file_to_image(d->mlx, d->imag.img_ground, \
+		d->img = mlx_xpm_file_to_image(d->mlx, d->imag.img_ground, \
 									&d->imag.img_wid, &d->imag.img_hei);
 	if (symb == 'C')
 		d->imag.img_ground = mlx_xpm_file_to_image(d->mlx, d->imag.img_exit, \
@@ -28,8 +30,10 @@ void	put_static_image(char symb, t_vars *d)
 	if (symb == 'E')
 	{
 		if (d->collect != 0)
-		d->imag.img_ground = mlx_xpm_file_to_image(d->mlx, d->imag.img_ground, \
+			d->imag.img_ground = mlx_xpm_file_to_image(d->mlx, d->imag.img_ground, \
 									&d->imag.img_wid, &d->imag.img_hei);
+		else
+			d->img = mlx_xpm_file_to_image()
 	}
 }
 
